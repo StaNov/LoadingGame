d3f9630a47d32578fa3209cfcf9317d43d70a51d
StaNov <stanovec@gmail.com>
2016-06-14 14:12:59 +0200

    added loading bar, trying to commit from visual studio

---
+++ Loading/Assets/LoadingBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingBar : MonoBehaviour {
+
+    void Awake() {
+
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
