c668a88a2df8147dad73e5ef144e4caae266e47c
StaNov <stanovec@gmail.com>
2016-06-15 09:10:10 +0200

    door knocking

---
+++ Loading/Assets/Door.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    public Text testText;
+
+    void Start() {
+        testText.gameObject.SetActive(false);
+    }
+
+    IEnumerator OnMouseDown() {
+        // TODO
+        testText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        testText.gameObject.SetActive(false);
+    }
+}
