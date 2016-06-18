15dea768eed111ea9eacc6785298f99b14710b9b
StaNov <stanovec@gmail.com>
2016-06-18 16:03:34 +0200

    ending labels

---
+++ Loading/Assets/EndScreen/EndingLabel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndingLabel : MonoBehaviour {
+
+    public LevelEnd levelEnd;
+
+    private Image image;
+
+    void Awake () {
+        image = GetComponent<Image>();
+    }
+
+    void Start () {
+        if (LevelEnder.levelEnd == levelEnd) {
+            Color c = image.color;
+            c.a = 1;
+            image.color = c;
+        }
+    }
+
+    void Update () {
+        
+    }
+}
