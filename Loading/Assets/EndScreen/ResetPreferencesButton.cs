d4f0c04e9f4f2ff37903158496ed443de613f7b5
StaNov <stanovec@gmail.com>
2016-06-23 18:53:17 +0200

    reset preferences debug button

---
+++ Loading/Assets/EndScreen/ResetPreferencesButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPreferencesButton : MonoBehaviour {
+
+    #if !DEBUG
+    void Start () {
+        Destroy(gameObject);
+        return;
+    }
+    #endif
+
+    public void ResetPreferences () {
+        PlayerPrefs.DeleteAll();
+    }
+}
