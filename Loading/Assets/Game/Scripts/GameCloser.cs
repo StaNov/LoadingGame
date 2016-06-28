d314863c3cb3ffcdb16b434d0305951fdb6a0734
StaNov <stanovec@gmail.com>
2016-06-28 21:41:14 +0200

    closing game with back button, resolves #63

---
+++ Loading/Assets/Game/Scripts/GameCloser.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCloser : MonoBehaviour {
+
+    private static GameCloser instance;
+
+    void Awake () {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("Quitting");
+            Application.Quit();
+        }
+    }
+}
