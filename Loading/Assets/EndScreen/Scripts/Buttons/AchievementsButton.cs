65cacf07839039768b09551075ed7d21e8b28770
StaNov <stanovec@gmail.com>
2016-06-28 22:12:13 +0200

    achievements button, resolves #70

---
+++ Loading/Assets/EndScreen/Scripts/Buttons/AchievementsButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementsButton : MonoBehaviour {
+
+    public void ShowAchievements () {
+        Social.ShowAchievementsUI();
+    }
+}
