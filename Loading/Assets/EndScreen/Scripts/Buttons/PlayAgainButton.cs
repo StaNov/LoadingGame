f1d8e22b093e2a0ef4b42ff62e2c77a3a976c37e
StaNov <stanovec@gmail.com>
2016-06-29 14:34:56 +0200

    unity ads implemented, resolves #71

---
+++ Loading/Assets/EndScreen/Scripts/Buttons/PlayAgainButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayAgainButton : MonoBehaviour {
+
+    public GameObject adLoadingPanel;
+
+    private float timeSinceAdLoadStarted;
+
+    void Start() {
+        adLoadingPanel.SetActive(false);
+    }
+
+    public void PlayAnAdAndRestart() {
+        StartCoroutine(PlayAnAdAndRestartCoroutine());
+    }
+
+    private IEnumerator PlayAnAdAndRestartCoroutine() {
+        adLoadingPanel.SetActive(true);
+        timeSinceAdLoadStarted = Time.time;
+
+        while (!Advertisement.IsReady()) {
+            if (Time.time - timeSinceAdLoadStarted > 5) {
+                Restart();
+                yield break;
+            }
+
+            yield return null;
+        }
+
+        var options = new ShowOptions { resultCallback = HandleShowResult };
+        Advertisement.Show(null, options);
+    }
+
+    private void HandleShowResult(ShowResult showResult) {
+        Restart();
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(0);
+    }
+}
