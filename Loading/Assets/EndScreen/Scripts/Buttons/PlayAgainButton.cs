801ec6d83abe5feba9e746cdb8e1fd9e35cd430a
StaNov <stanovec@gmail.com>
2016-07-11 21:07:20 +0200

    ad shown after each replay if 3 or more endings have been unlocked, resolves #105

--- Loading/Assets/EndScreen/Scripts/Buttons/PlayAgainButton.cs
+++ Loading/Assets/EndScreen/Scripts/Buttons/PlayAgainButton.cs
@@ -2,43 +2,45 @@
 using UnityEngine.Advertisements;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
 
 public class PlayAgainButton : MonoBehaviour {
 
     public GameObject adLoadingPanel;
-
-    private float timeSinceAdLoadStarted;
 
     void Start() {
         adLoadingPanel.SetActive(false);
     }
 
     public void PlayAnAdAndRestart() {
-        StartCoroutine(PlayAnAdAndRestartCoroutine());
-    }
-
-    private IEnumerator PlayAnAdAndRestartCoroutine() {
-        adLoadingPanel.SetActive(true);
-        timeSinceAdLoadStarted = Time.time;
-
-        while (!Advertisement.IsReady("rewardedVideo")) {
-            if (Time.time - timeSinceAdLoadStarted > 5) {
-                Restart();
-                yield break;
-            }
-
-            yield return null;
+        if (! AtLeastXEndingsWereUnlocked(3) || !Advertisement.IsReady("video")) {
+            Restart();
+            return;
         }
 
+        adLoadingPanel.SetActive(true);
         var options = new ShowOptions { resultCallback = HandleShowResult };
-        Advertisement.Show("rewardedVideo", options);
+        Advertisement.Show("video", options);
+    }
+
+    private bool AtLeastXEndingsWereUnlocked(int x) {
+        int count = 0;
+
+        foreach (LevelEnd levelEnd in Enum.GetValues(typeof(LevelEnd))) {
+            if (PlayerPrefs.HasKey(levelEnd.ToString())) {
+                count++;
+            }
+        }
+        
+        return count >= x;
     }
 
     private void HandleShowResult(ShowResult showResult) {
+        adLoadingPanel.SetActive(false);
         Restart();
     }
 
-    public void Restart() {
+    private void Restart() {
         SceneManager.LoadScene(0);
     }
 }
