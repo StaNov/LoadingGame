85fc47e1a04933bd99bbddb332fa5b0644adb785
StaNov <info@stanov.cz>
2020-11-04 21:08:53 +0100

    Removed ads

--- Loading/Assets/EndScreen/Scripts/Buttons/PlayAgainButton.cs
+++ Loading/Assets/EndScreen/Scripts/Buttons/PlayAgainButton.cs
@@ -1,8 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.Advertisements;
 using UnityEngine.SceneManagement;
-using System.Collections;
-using System;
 
 public class PlayAgainButton : MonoBehaviour {
 
@@ -13,34 +10,6 @@ public class PlayAgainButton : MonoBehaviour {
     }
 
     public void PlayAnAdAndRestart() {
-        if (! AtLeastXEndingsWereUnlocked(3) || !Advertisement.IsReady("video")) {
-            Restart();
-            return;
-        }
-
-        adLoadingPanel.SetActive(true);
-        var options = new ShowOptions { resultCallback = HandleShowResult };
-        Advertisement.Show("video", options);
-    }
-
-    private bool AtLeastXEndingsWereUnlocked(int x) {
-        int count = 0;
-
-        foreach (LevelEnd levelEnd in Enum.GetValues(typeof(LevelEnd))) {
-            if (PlayerPrefs.HasKey(levelEnd.ToString())) {
-                count++;
-            }
-        }
-        
-        return count >= x;
-    }
-
-    private void HandleShowResult(ShowResult showResult) {
-        adLoadingPanel.SetActive(false);
-        Restart();
-    }
-
-    private void Restart() {
         SceneManager.LoadScene(0);
     }
 }
